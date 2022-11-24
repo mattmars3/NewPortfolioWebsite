997e98e0ee7f8aad30732bc21ef7493307190dc6
mattmars3 <mjmarsico@hotmail.com>
2022-11-24 12:38:24 -0600

    show more button

--- js/showMore.js
+++ js/showMore.js
@@ -1,4 +1,23 @@
-// show the text when button is clicked
+
 function showMore(elementId) {
-  // make element visible
+  const pTagCollection = document.getElementById(elementId).querySelectorAll('p.moreContent');
+
+  const showMoreButton = document.getElementById(elementId).getElementsByClassName('showMoreButton')[0];
+  
+  let visibility = '';
+  let buttonText = '';
+  if (showMoreButton.innerText == 'Show More') {
+    buttonText = 'Hide Info'
+    visibility = 'block'
+  } else {
+    buttonText = 'Show More'
+    visibility = 'none'
+  }
+
+  for (let pTag in pTagCollection) {
+    pTagCollection[pTag].style.display = visibility; 
+    showMoreButton.innerText = buttonText;
+  }
+
+  // fix css issue
 }
