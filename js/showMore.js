cb1ab2bb99269b060a8cc0cf554aadb9e662f73c
= <mjmarsico@hotmail.com>
2022-12-09 21:49:17 -0600

    worked on showmore button

--- js/showMore.js
+++ js/showMore.js
@@ -14,16 +14,33 @@ function showMore(elementId) {
   let buttonText = "";
 
   // if button says show more then
-  if (showMoreButton.innerText == "+") {
-    buttonText = "-";
+  if (showMoreButton.innerText == "Show More") {
+    buttonText = "Show Less";
     visibility = "block";
   } else {
-    buttonText = "+";
+    buttonText = "Show More";
     visibility = "none";
   }
 
   for (let pTag in pTagCollection) {
     showMoreButton.innerText = buttonText;
+    let out = pTagCollection[pTag].childNodes; 
+    console.log(out)
+    /*  
     pTagCollection[pTag].setAttribute("style", `display:${visibility};`);
+    */
+  }
+  
+  // re-apply all the css cuz it won't work for some reason
+  const showMoreButtons = document.getElementsByClassName('showMoreButton');
+  for (let showMoreButton in showMoreButtons) {
+    showMoreButtons[showMoreButton].setAttribute('style', 'font-family: "Impact"')
+    console.log('completed font change')
+  }
+}
+
+function show(elementId) {
+  const showMores = document.body.querySelector(elementId).querySelectorAll('.moreContent');
+  for (let i in showMores) {
   }
 }
